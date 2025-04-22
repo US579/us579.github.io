d1136f5ef2cf27d087b73ca6e30cca0c8afac128
Steven Liu <steven.l@covergenius.com>
2025-04-22 22:07:20 +1000

    chore: update styles

---
+++ precache-manifest.d50f92cfd86868e33a9f4658ee1e7efc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b6c0045f0472b4431d8826e85d89c0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a2376db1c49abde1b88",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "a3f00daf3af93cebf00e",
+    "url": "/static/js/2.da93deb0.chunk.js"
+  },
+  {
+    "revision": "2c10491364e9bdf48063",
+    "url": "/static/js/3.fc5cbf83.chunk.js"
+  },
+  {
+    "revision": "4a2376db1c49abde1b88",
+    "url": "/static/js/main.f4b5f80a.chunk.js"
+  },
+  {
+    "revision": "a50475a1f486eef27c5a",
+    "url": "/static/js/runtime~main.4414e1f1.js"
+  },
+  {
+    "revision": "1b86963efd5aa37c3d409070cbca0ff0",
+    "url": "/static/media/avator.1b86963e.png"
+  }
+]);
